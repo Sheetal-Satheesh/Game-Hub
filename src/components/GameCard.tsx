c8746b1efec5d28a2259303e55df6f8137d6c8ad
Sheetal-Satheesh <sheetalsatheesh1@gmail.com>
2024-05-27 17:10:21 +0200

    Show Loading Skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, Image, Heading, CardBody, Text, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,16 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <>
+      <Card borderRadius={10} overflow="hidden">
+        <Skeleton height={60}></Skeleton>
+        <CardBody>
+          <SkeletonText></SkeletonText>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default GameCardSkeleton;
